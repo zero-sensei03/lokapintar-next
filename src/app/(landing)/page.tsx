407200ae0f3d0aa4a9d13d898b297a13c29db361
zero-sensei03 <raflinasution.mr@gmail.com>
2026-08-19 09:49:09 +0700

    initial commit

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Camera } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Camera />
+    </div>
+  );
+}
